c9a9d34f5668205384b53459d62a1c471b53423b
aayush-aryan <log2aayusharyan@gmail.com>
2022-10-27 02:08:18 +0530

    added. validations for reset components

--- src/app/reset/reset.component.ts
+++ src/app/reset/reset.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reset',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset.component.scss']
 })
 export class ResetComponent implements OnInit {
+  resetForm !: FormGroup
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.resetForm=this.formBuilder.group({
+      newpassword: ['', [Validators.required, Validators.pattern('(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-])[a-zA-Z0-9!@#$%^&*()_+=-]{8,}$'), Validators.minLength(8)]],
+      confirmnewPassword: ['',Validators.required]
+    });
   }
-
+  onSubmit(){}
 }
