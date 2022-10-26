b170c726812287fc6ad258c8ce4c9237676e346b
aayush-aryan <log2aayusharyan@gmail.com>
2022-10-27 01:38:04 +0530

    added. validations for register components

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  registerForm !: FormGroup;
+  constructor(private formBuilder: FormBuilder) { 
+   
   }
 
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      firstname: ['', [Validators.required, Validators.pattern('[A-Z]{1}[a-z]{2,}$'), Validators.minLength(3)]],
+      lastname: ['', [Validators.required, Validators.pattern('[A-Z]{1}[a-z]{2,}$'), Validators.minLength(3)]],
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern('(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-])[a-zA-Z0-9!@#$%^&*()_+=-]{8,}$'), Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required]
+    });
+  }
+  onSubmit() {}
 }
+ 
