0deb497db516ac1fc17552f7d03943e0f1a57829
aayush-aryan <log2aayusharyan@gmail.com>
2022-10-26 22:03:52 +0530

    added. routing is added for register,signup,forget ad reset component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ForgotComponent } from './forgot/forgot.component';
+import { ResetComponent } from './reset/reset.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'register', component:RegisterComponent},
+  {path:'signin',component:LoginComponent},
+  {path:'forgot',component:ForgotComponent},
+  {path:'reset',component:ResetComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
